d013e0c46c7f1d0c2fa0a34037af1b1c24175c95
Abdel <112128079+DjeAbdel0@users.noreply.github.com>
2024-04-23 11:43:31 -0400

    test_parallax

--- script/script.js
+++ script/script.js
@@ -19,9 +19,9 @@ function handleScroll() {
   
 
 /*------------------------------------------------------------------
-Chapitre 2 : titre Anim
+Chapitre 2
 --------------------------------------------------------------------*/ 
-
+/*---Anim Text---*/
   gsap.to(".text, .outline-text", {
     scrollTrigger: {
       trigger: ".text, .outline-text",
@@ -32,5 +32,34 @@ Chapitre 2 : titre Anim
     y: '15vh',
   });
 
+/*---Parallax---*/
+const parallax = gsap.timeline({
+  scrollTrigger: {
+      trigger: ".parralax",
+      start: "250vh center",  
+      end: "bottom top",   
+      scrub: true,
+      markers: true,
+      ease: "none",
+  }
+});
+
+
+parallax.to('.mont-gauche', { y: -80 }, '<') 
+.to('.mont-droite',  { y: -80 }, '<0.2')
+.to('.arbres',  { y: -120 }, '<0.2')
+
+
+
+
+window.addEventListener('scroll', function() {
+  var maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+  var currentScroll = window.scrollY;
+  var scrollFraction = currentScroll / maxScroll;
+  var horizontalShift = 15 * scrollFraction; // Shift right by up to 15vw
+
+  var diver = document.querySelector('.ss-diver');
+  diver.style.transform = `translate(${(-50 + horizontalShift)}%, -50%)`;
+});
   
   
